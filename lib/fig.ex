2c5e63fa6489635ea772585db318e7530c815941
Dax Ravi <d@ironbay.digital>
2017-09-11 17:48:34 -0400

    Different API

--- lib/fig.ex
+++ lib/fig.ex
@@ -5,9 +5,16 @@ defmodule Fig do
 		end
 	end
 
-	defmacro defconfig(name, opts = [env: env, default: default]) do
+	defmacro defconfig(opts) do
+		case Enum.at(opts, 0) do
+			{name, {env, default}} -> Fig.create(name: name, env: env, default: default)
+			{name, value} -> Fig.create(name: name, env: name |> Atom.to_string |> String.upcase, default: value)
+		end
+	end
+
+	def create([name: name, env: env, default: default]) do
 		quote do
-			def unquote(name) do
+			def unquote(name)() do
 				unquote(env)
 				|> System.get_env || unquote(default)
 			end
@@ -18,6 +25,6 @@ end
 defmodule Fig.Example do
 	import Fig
 
-	defconfig foo, env: "FOO", default: "bar"
+	defconfig foo: "bar"
 
 end
