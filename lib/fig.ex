4b7a509e0ed68b179f861d7a18e2eaa3e72e7803
Dax Ravi <d@ironbay.digital>
2017-09-11 22:22:52 -0400

    Fig

--- lib/fig.ex
+++ lib/fig.ex
@@ -5,6 +5,44 @@ defmodule Fig do
 		end
 	end
 
+	defmacro config([{root, value}]) do
+		{map, _} =
+			value
+			|> Code.eval_quoted
+
+		flattened =
+			map
+			|> Dynamic.flatten
+			|> Enum.map(fn {key, value} -> [key, [root] ++ key, value] end)
+			|> Enum.map(fn [key, full, value] -> [key, full |> Enum.join("_") |> String.to_atom, value] end)
+
+		result =
+			flattened
+			|> Enum.map(fn [_, full, value] ->
+					uppercase =
+						full
+						|> Atom.to_string
+						|> String.upcase
+				quote do
+					def unquote(full)() do
+						System.get_env(unquote(uppercase)) || unquote(value)
+					end
+				end
+			end)
+
+		[
+			quote do
+				def unquote(root)() do
+					unquote(flattened)
+					|> Enum.reduce(%{}, fn [key, full, _], collect ->
+						Dynamic.put(collect, key, apply(__MODULE__, full, []))
+					end)
+				end
+			end
+			| result
+		]
+	end
+
 	defmacro defconfig(opts) do
 		case Enum.at(opts, 0) do
 			{name, {env, default}} -> Fig.create(name: name, env: env, default: default)
@@ -25,6 +63,10 @@ end
 defmodule Fig.Example do
 	import Fig
 
-	defconfig foo: "bar"
+	config test: %{
+		this: %{
+			is: "lol"
+		}
+	}
 
 end
